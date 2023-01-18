ef91c9605545697c2b8f2e79832019e3182c1f8a
Milton Carlos Katoo <mckatoo@gmail.com>
2023-01-17 22:54:40 -0300

    about page entity

---
+++ src/domain/about-page/AboutPage.test.ts
@@ -0,0 +1,14 @@
+import AboutPage from './AboutPage'
+
+describe("About Page Class", () => {
+  it("should create new instance", () => {
+    const mockData = {
+      title: "Titulo da pagina",
+      description: "Descrição da pagina",
+      skills: ["nodejs", "git"],
+    };
+    const aboutPage = new AboutPage(mockData);
+
+    expect(expect.objectContaining(aboutPage)).toStrictEqual(mockData);
+  });
+});
