f0f02804fdaf2b76a6ff3cc3e2283e5221364b1d
Milton Carlos Katoo <mckatoo@gmail.com>
2023-01-19 18:43:56 -0300

    about page use case implemented and tested

---
+++ src/application/IAboutPageApplication.ts
@@ -0,0 +1,11 @@
+import { AboutPageProps } from "@/domain/about-page/AboutPage";
+import { SkillProps } from "@/domain/skill/Skill";
+
+export type AboutPageWithSkills = AboutPageProps & { skills: SkillProps[] };
+
+export interface IAboutPageApplication {
+  createAboutPage(page: AboutPageWithSkills): Promise<void>;
+  getAboutPage(): Promise<AboutPageWithSkills>;
+  deleteAboutPage(): Promise<void>;
+  deleteSkill(title: string): Promise<void>
+}
