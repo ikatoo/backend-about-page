100233788c8ee633a4d1182ba8d9310de3ea02ba
Milton Carlos Katoo <mckatoo@gmail.com>
2023-01-20 23:51:17 -0300

    post route to create about page

--- src/infra/express/routes/aboutPage/aboutPage.test.ts
+++ src/infra/express/routes/aboutPage/aboutPage.test.ts
@@ -1,11 +1,56 @@
-import { describe, test } from "vitest";
+import { AboutPageWithSkills } from "@/application/IAboutPageApplication";
+import postgres from "@/infra/database/postgres";
+import request from "supertest";
+import { describe, expect, test } from "vitest";
 
-describe.skip("Express - About Page", () => {
-  test("should create about page", async () => {});
+import app from "../../app";
 
-  test("should get about page data", async () => {});
+describe("Express - About Page", () => {
+  const mock: AboutPageWithSkills = {
+    title: "title test supertest",
+    description: "desc test supertest",
+    skills: [{ title: "docker" }, { title: "git" }, { title: "nodejs" }],
+    avatarURL: "avatar url",
+    avatarALT: "alt of the avatar",
+  };
 
-  test("should update about page", async () => {});
+  test("should create about page", async () => {
+    await postgres.none('delete from about_page')
+    await postgres.none('delete from skills')
+    
+    const response = await request(app).post("/about").send(mock);
+    const aboutPage = await postgres.one("select * from about_page");
+    const skills = await postgres.manyOrNone(
+      "select * from skills order by title"
+    );
 
-  test("should delete about page", async () => {});
+    expect(response.status).toBe(201);
+    expect(aboutPage).toStrictEqual({
+      title: mock.title,
+      description: mock.description,
+      avatar_url: mock.avatarURL,
+      avatar_alt: mock.avatarALT,
+    });
+    expect(skills).toStrictEqual(mock.skills);
+  });
+
+  test.skip("should get about page data", async () => {
+    const exist = await postgres.manyOrNone("select * from about-page");
+    !exist &&
+      (await postgres.none(
+        "insert into about-page (title,description, avatar_url, avatar_alt values ($1,$2,$3,$4)",
+        [mock.title, mock.description, mock.avatarURL, mock.avatarALT]
+      ));
+    const response = await request(app).get("/about").send();
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(mock);
+  });
+
+  test.skip("should update about page", async () => {
+    const response = await request(app).get("/about").send();
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(mock);
+  });
+
+  test.skip("should delete about page", async () => {});
 });
