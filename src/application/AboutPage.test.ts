869c149d2bc65f1657442afb9246efcf834f4067
Milton Carlos Katoo <mckatoo@gmail.com>
2023-01-21 19:38:30 -0300

    🤡 (test): shared mock from infra http structure
    this mock should be used for applications with use this api

--- src/application/AboutPage.test.ts
+++ src/application/AboutPage.test.ts
@@ -1,52 +1,38 @@
-import PostgresAboutPage from "@/infra/database/about-page/PostgresAboutPage";
-import PostgresSkills from "@/infra/database/skills/PostgresSkills";
-import { beforeAll, describe, expect, test } from "vitest";
-import AboutPage from "./AboutPage";
-import { AboutPageWithSkills } from "./IAboutPageApplication";
-import postgres from "@/infra/database/postgres";
+import PostgresAboutPage from '@/infra/database/about-page/PostgresAboutPage'
+import postgres from '@/infra/database/postgres'
+import PostgresSkills from '@/infra/database/skills/PostgresSkills'
+import { aboutPageMock } from '@/shared/aboutPageMock'
+import { beforeAll, describe, expect, test } from 'vitest'
+import AboutPage from './AboutPage'
 
-describe("About Page Use Case test", () => {
-  const aboutPageMock: AboutPageWithSkills = {
-    title: "About page title",
-    description: "About page desc",
-    skills: [
-      { title: "docker" },
-      { title: "jest" },
-      { title: "tdd" },
-      { title: "vitest" },
-    ],
-    avatarURL: "url of the avatar",
-    avatarALT: "this is a avatar",
-  };
-  const aboutPageRepository = new PostgresAboutPage();
-  const skillsRepository = new PostgresSkills();
-  const aboutPageUseCase = new AboutPage(aboutPageRepository, skillsRepository);
+describe('About Page Use Case test', () => {
+  const aboutPageRepository = new PostgresAboutPage()
+  const skillsRepository = new PostgresSkills()
+  const aboutPageUseCase = new AboutPage(aboutPageRepository, skillsRepository)
 
   beforeAll(async () => {
-    await postgres.none("delete from about_page");
-    await postgres.none("delete from skills");
-  });
+    await postgres.none('delete from about_page')
+    await postgres.none('delete from skills')
+  })
 
-  test("should create a About Page", async () => {
+  test('should create a About Page', async () => {
     await expect(
       aboutPageUseCase.createAboutPage(aboutPageMock)
-    ).resolves.not.toThrowError();
-  });
+    ).resolves.not.toThrowError()
+  })
 
-  test("should get the About Page", async () => {
-    await aboutPageUseCase.getAboutPage();
-  });
+  test('should get the About Page', async () => {
+    await aboutPageUseCase.getAboutPage()
+  })
 
-  test("should delete a Skill", async () => {
+  test('should delete a Skill', async () => {
     await expect(
-      aboutPageUseCase.deleteSkill("jest")
-    ).resolves.not.toThrowError();
-  });
+      aboutPageUseCase.deleteSkill('jest')
+    ).resolves.not.toThrowError()
+  })
 
-  test("should delete the About Page", async () => {
-    await expect(
-      aboutPageUseCase.deleteAboutPage()
-    ).resolves.not.toThrowError();
-    await expect(aboutPageUseCase.getAboutPage()).resolves.toBeUndefined();
-  });
-});
+  test('should delete the About Page', async () => {
+    await expect(aboutPageUseCase.deleteAboutPage()).resolves.not.toThrowError()
+    await expect(aboutPageUseCase.getAboutPage()).resolves.toBeUndefined()
+  })
+})
